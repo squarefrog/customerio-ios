6ce5b3aa07c3938941c30f499e6d519675b6ed92
Taylor Williamson <taylor.williamson@thebeansgroup.com>
2026-01-16 12:05:48 +0000

    Remove typeliases to internal common

--- Sources/DataPipeline/Type/Aliases.swift
+++ Sources/DataPipeline/Type/Aliases.swift
@@ -6,8 +6,8 @@ import Foundation
  Contains public aliases to expose public structures/protocols from the 'CioInternalCommon' module
  to the 'DataPipeline' module.
  */
-public typealias Metric = CioInternalCommon.Metric
-public typealias CustomerIO = CioInternalCommon.CustomerIO
-public typealias CioLogLevel = CioInternalCommon.CioLogLevel
-public typealias Region = CioInternalCommon.Region
-public typealias ScreenView = CioInternalCommon.ScreenView
+//public typealias Metric = CioInternalCommon.Metric
+//public typealias CustomerIO = CioInternalCommon.CustomerIO
+//public typealias CioLogLevel = CioInternalCommon.CioLogLevel
+//public typealias Region = CioInternalCommon.Region
+//public typealias ScreenView = CioInternalCommon.ScreenView
